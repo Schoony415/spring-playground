05c65ad576808bd62ebce6c589421d9ba6954a84
Schoony415 <joshua.schoonover@austincc.edu>
2021-02-22 15:46:20 -0600

    First upload

---
+++ src/main/java/com/example/demo/FileManager.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileManager {
+    public static String readFileAsString(String fileName) {
+        String text = "File grabber didn't work!";
+        try {
+            text = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+    //Read more: https://www.java67.com/2016/08/how-to-read-text-file-as-string-in-java.html#ixzz6nDwSkUVT
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//trying to make a 404 page
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        String mystring = "Default: Hello World!";
+        //path to file on my computer without the file name
+        String path = "/Users/j2153034/Documents/GitHub/Springdemo/src/main/java/com/example/demo";
+        //just my file name with the leading slash
+        String filename = "/TestText.txt";
+        mystring = FileManager.readFileAsString(path+filename);
+
+        return mystring;
+        //return "Hello Baby";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "About page";
+    }
+    @GetMapping("/a")//trying to make two things goto 1 page
+    public String about2(){return about();}
+
+    //@GetMapping("/error")
+    //@ExceptionHandler
+    public String error(){
+        return "THIS IS MY 404 PAGE, GET WRECKED NOOB!";
+    }
+}
