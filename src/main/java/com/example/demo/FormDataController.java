725b869a2ff4fb5e2ac484505c8f596fcdce23f2
Schoony415 <joshua.schoonover@austincc.edu>
2021-02-24 13:15:10 -0600

    Create FormDataController.java

---
+++ src/main/java/com/example/demo/FormDataController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.galvanize.RequestObjects.Search;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+//this is straigt from an example
+@RestController
+@RequestMapping("/rb")
+public class FormDataController {
+
+    @PostMapping("/individual-example")
+    public String getIndividualParams(@RequestParam String from, @RequestParam("q") String query) {
+        return String.format("q:%s from:%s", query, from);
+    }
+
+    @PostMapping(value = "/map-example", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String getMapParams(@RequestParam Map<String, String> formData) {
+        return formData.toString();
+    }
+
+    @PostMapping(value = "/object-example", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String getObjectParams(Search search) {
+        return search.toString();
+    }
+
+    @PostMapping("/string-example")
+    public String getRawString(@RequestBody String rawBody) {
+        return rawBody;
+    }
+
+}
